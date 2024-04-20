d55de550d88c740b38134f8cc408afcf5adb4425
claussimar <dasunheimliche7@gmail.com>
2024-04-20 00:36:11 -0300

    optimizacion del hook useAllPosts

--- src/hooks/useAllPosts.ts
+++ src/hooks/useAllPosts.ts
@@ -1,43 +1,45 @@
 import { getAllPosts } from "@/lib/services";
 import { POSTS_PER_PAGE } from "@/lib/constants";
 import { Post } from "@/types";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 
 const useAllPosts = () => {
   const [posts, setPosts] = useState<Post[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [totalPages, setTotalPages] = useState<number>(0);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const fetchedPosts = await getAllPosts();
-        setPosts(fetchedPosts);
-        setTotalPages(Math.ceil(fetchedPosts.length / POSTS_PER_PAGE));
+        const response = await getAllPosts();
+        setPosts(response);
         setIsLoading(false);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-        } else {
-          setError("Error desconocido");
-        }
+      } catch (error: any) {
+        setError(error.message || "Error desconocido");
         setIsLoading(false);
       }
     };
 
     fetchPosts();
-  }, [POSTS_PER_PAGE]);
+  }, []);
 
-  const getPostsByPage = (page: number): Post[] => {
-    const startIndex = (page - 1) * POSTS_PER_PAGE;
-    const endIndex = startIndex + POSTS_PER_PAGE;
-    return posts.slice(startIndex, endIndex);
-  };
+  const totalPages = useMemo(
+    () => Math.ceil(posts.length / POSTS_PER_PAGE),
+    [posts]
+  );
 
-  const arrayPages = Array.from(
-    { length: totalPages },
-    (_, index) => index + 1
+  const getPostsByPage = useCallback(
+    (page: number) => {
+      const startIndex = (page - 1) * POSTS_PER_PAGE;
+      const endIndex = startIndex + POSTS_PER_PAGE;
+      return posts.slice(startIndex, endIndex);
+    },
+    [posts]
+  );
+
+  const arrayPages = useMemo(
+    () => Array.from({ length: totalPages }, (_, index) => index + 1),
+    [totalPages]
   );
 
   return { posts, isLoading, error, arrayPages, getPostsByPage };
